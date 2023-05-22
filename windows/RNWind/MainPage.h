2a4231b2d5fbe2b0f4d9028e90616d474bea347a
Evgenij <58779583+j238267431@users.noreply.github.com>
2023-05-22 15:55:49 +0300

    first

---
+++ windows/RNWind/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::RNWind::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::RNWind::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
